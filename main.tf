7e19de64eb3d3d304b6ee7ed40ae1f60e2e42da4
Azeemuddin Mohammed <azeem_parvez@hotmail.com>
2023-09-21 17:01:33 +0100

    NAT Gateway Subnet Added

--- main.tf
+++ main.tf
@@ -17,17 +17,18 @@ resource "azurerm_virtual_network" "hiperdist-vnet1" {
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
   subnet {
-    name           = "subnet1"
+    name           = "frontend"
     address_prefix = "10.0.0.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.1.0/24"
     security_group = azurerm_network_security_group.hiperdist-rg1-sg.id
   }
 
+  subnet {
+    name           = "GatewaySubnet"
+    address_prefix = "10.0.1.0/24"
+  
+  }
+
   tags = {
-    environment = "Production"
+    environment = "Development"
   }
 }
