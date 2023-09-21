63afc278538e3e7e05dfd719bf5190a66f2c0c10
Azeemuddin Mohammed <azeem_parvez@hotmail.com>
2023-09-21 17:21:25 +0100

    code fmt

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
-resource "azurerm_resource_group" "hiperdist-rg1" {
-  name     = "app1"
+resource "azurerm_resource_group" "rg1" {
+  name     = "hiperdist-app1"
   location = "West Europe"
 }
 
-resource "azurerm_network_security_group" "hiperdist-rg1-sg" {
+resource "azurerm_network_security_group" "sg1" {
   name                = "app-sg1"
-  location            = azurerm_resource_group.hiperdist-rg1.location
-  resource_group_name = azurerm_resource_group.hiperdist-rg1.name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
 }
 
 resource "azurerm_virtual_network" "hiperdist-vnet1" {
   name                = "vnet1"
-  location            = azurerm_resource_group.hiperdist-rg1.location
-  resource_group_name = azurerm_resource_group.hiperdist-rg1.name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
   subnet {
     name           = "frontend"
     address_prefix = "10.0.0.0/24"
-    security_group = azurerm_network_security_group.hiperdist-rg1-sg.id
+    security_group = azurerm_network_security_group.sg1.id
   }
 
   subnet {
     name           = "GatewaySubnet"
     address_prefix = "10.0.1.0/24"
-  
+
   }
 
   tags = {
